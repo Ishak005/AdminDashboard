906a64a7eda97df08f6de8fd9b44908df9020acf
Ishak005 <ishaktwahir@gmail.com>
2024-07-26 23:49:23 +0300

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,51 @@
-import './index.css'
-import { ColorModeContext, useMode } from './Theme'
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import Topbar from './Scenes/Global/TopBar'
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./Scenes/Global/TopBar";
+import Sidebar from "./Scenes/Global/SideBar";
+// import Dashboard from "./Scenes/Dashboard";
+// import Team from "./Scenes/team";
+// import Invoices from "./Scenes/invoices";
+// import Contacts from "./Scenes/contacts";
+// import Bar from "./Scenes/bar";
+// import Form from "./Scenes/form";
+// import Line from "./Scenes/line";
+// import Pie from "./Scenes/pie";
+// import FAQ from "./Scenes/faq";
+// import Geography from "./Scenes/geography";
+// import { CssBaseline, ThemeProvider } from "@mui/material";
+// import { ColorModeContext, useMode } from "./Theme";
+// import Calendar from "./Scenes/calendar/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
-        <CssBaseline/>
-   <div className="app">
-    <main className="content">
-      <Topbar />
-    </main>
-   </div>
-   </ThemeProvider>
-   </ColorModeContext.Provider>
-  )
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/geography" element={<Geography />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
-export default App
+export default App;
--- src/Scenes/Global/SideBar.jsx
+++ src/Scenes/Global/SideBar.jsx
@@ -1,4 +1,22 @@
-import React from 'react'
+import { useState } from 'react'
+import { Menu,ProSidebar,MenuItem } from 'react-pro-sidebar'
+// import 'react-pro-sidebar/dist/css/styles.css';
+import { Box, IconButton, Typography, useTheme } from '@mui/material'
+import { Link } from 'react-router-dom'
+import { tokens } from '../../Theme'
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactPageOutlinedIcon from "@mui/icons-material/ContactPageOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlinedIcon from "@mui/icons-material/PieChartOutlined";
+import TimeLineOutlinedIcon from "@mui/icons-material/TimeLineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+
 
 const SideBar = () => {
   return (
